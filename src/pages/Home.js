92e7378bbc2c28b58fdd4ba3d0d68a96b050ce1a
Carolina Figueroa <andreacarolina.f28@gmail.com>
2022-12-12 14:31:16 -0600

    Componentes para las dos vistas

---
+++ src/pages/Home.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { Footer } from "../components/Footer";
+import { useNavigate } from "react-router-dom";
+import { Banner } from "../components/Banner";
+import { Skills } from "../components/Skills";
+import { Projects } from "../components/Projects";
+import { Contact } from "../components/Contact";
+import { Controller } from "react-bootstrap-icons";
+import NavBar from "../components/NavBar";
+
+function Home() {
+    console.log("Hello");
+    const navigate = useNavigate();
+    return (
+        <div className="Home-container">
+            <h1>jbiub</h1>
+            <NavBar />
+        </div>
+    );
+}
+
+export default Home;
