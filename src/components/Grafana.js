1d735cb42b78b45d493e455e505edbeadadf70a4
Brandon-Rivera <A01424338@itesm.mx>
2022-12-13 19:27:34 -0600

    Grafana css

--- src/components/Grafana.js
+++ src/components/Grafana.js
@@ -1,9 +1,14 @@
 import React from 'react'
+import { Container, Row, Col } from "react-bootstrap";
+import "./Grafana.css";
 
 export const Grafana = () => {
   return (
-    <div>
-        Grafana
-    </div>
+    <section className="grafana">
+      <Container className='center'>
+        <h1>Ejemplo de Iframe</h1>
+        <iframe src="https://www.youtube.com/embed/uXWycyeTeCs" width={1000} height={500}></iframe>
+      </Container>
+    </section>
   )
 }
