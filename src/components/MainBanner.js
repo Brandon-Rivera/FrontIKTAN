a37576effdca8f9c92891d8d5eb8af9500fa2bb9
Carolina Figueroa <andreacarolina.f28@gmail.com>
2022-12-14 11:06:51 -0600

    Try 1 MainBanner

--- src/components/MainBanner.js
+++ src/components/MainBanner.js
@@ -1,11 +1,16 @@
 import { useNavigate } from "react-router-dom";
 import "./MainBanner.css";
 import iktanLogo from '../assets/img/iktanLogo.png';
+import tecLogo from '../assets/img/itesm-icon.svg';
+
 
 export const MainBanner = () => {
     return(
         <div className="MainBanner-container">
+            <h2>Welcome!</h2>
+            <h1>IKTAN TEAM 2022 - 2023</h1>
             <img className="logo-iktan" src={iktanLogo} alt="Logo" />
+            <img className="logo-tec" src={tecLogo} alt="Logo" />
         </div>
     );
 }
