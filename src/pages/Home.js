7193b6fc28614046dd8a665a8ff530181e5df7fa
Carolina Figueroa <andreacarolina.f28@gmail.com>
2022-12-14 11:06:12 -0600

    Agregue MainBanner

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -7,6 +7,7 @@ import { Projects } from "../components/Projects";
 import { Contact } from "../components/Contact";
 import { Controller } from "react-bootstrap-icons";
 import { NavBar } from "../components/NavBar";
+import { MainBanner } from "../components/MainBanner";
 
 function Home() {
     console.log("Hello");
@@ -14,6 +15,7 @@ function Home() {
     return (
         <div className="Home-container">
             <NavBar />
+            <MainBanner/>
             <Banner/>
             <Projects/>
             <Footer/>
