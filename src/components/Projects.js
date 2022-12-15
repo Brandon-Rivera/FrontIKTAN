28a2589a3044515a84cab7e41ab3ecf3a27729ce
Carolina Figueroa <andreacarolina.f28@gmail.com>
2022-12-15 13:24:08 -0600

    Galeria nueva

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -73,12 +73,10 @@ export const Projects = () => {
 
                     </Tab.Pane>
                     <Tab.Pane eventKey="second">
-                      <h2>Mission</h2>
-                      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque quam, quod neque provident velit, rem explicabo excepturi id illo molestiae blanditiis, eligendi dicta officiis asperiores delectus quasi inventore debitis quo.</p>
+
                     </Tab.Pane>
                     <Tab.Pane eventKey="third">
-                      <h2>Vision</h2>
-                      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque quam, quod neque provident velit, rem explicabo excepturi id illo molestiae blanditiis, eligendi dicta officiis asperiores delectus quasi inventore debitis quo.</p>
+                      
                     </Tab.Pane>
                     <Tab.Pane eventKey="fourth">
                     <Row>
